52f6b8e2c6b0c66c8cf9e4b75b0cdeb75b7aef1c
AndresssaAB <alvesaborges@gmail.com>
2020-04-02 22:54:21 -0400

    Add project files.

---
+++ WebApplication1.WebAPI/Modals/HeroiBatalha.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.WebAPI.Modals
+{
+    public class HeroiBatalha
+    {
+        public int HeroiId { get; set; }
+        public Heroi Heroi { get; set; }
+        public int BatalhaId { get; set; }
+        public Batalha Batalha { get; set; }
+        
+        
+    }
+}
